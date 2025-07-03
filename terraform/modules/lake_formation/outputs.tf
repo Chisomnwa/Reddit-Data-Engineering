466ec50f4f71c96020827955a63cf9c358093464
Chisom Nnamani <nnamanichisomnpc@gmail.com>
2025-07-03 04:44:24 +0100

    Remove folder from Git tracking

--- terraform/modules/lake_formation/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "database_permissions_id" {
-  description = "The ID of the Lake Formation database permissions"
-  value       = aws_lakeformation_permissions.grant_select_to_redshift.id
-}
-
-output "table_permissions_id" {
-  description = "The ID of the Lake Formation table permissions"
-  value       = aws_lakeformation_permissions.grant_select_to_redshift_tables.id
-}
